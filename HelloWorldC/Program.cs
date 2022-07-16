ec4fcc37ede95a834e33f12076bf1907ac92f8e2
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2022-07-16 11:23:49 -0400

    Add project files.

---
+++ HelloWorldC/Program.cs
@@ -0,0 +1,17 @@
+﻿
+divisibleByThree();
+static void divisibleByThree()
+{
+    int sum = 0;
+    for (int index = 1; index <= 20; index++)
+    {
+        var names = new List<string> { "<name>", "Ana", "Felipe" };
+        foreach (var name in names)
+        {
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+        }
+    }
+
+    Console.WriteLine($"The sum of all numbers divisible by 3 is {sum}!");
+}
+
